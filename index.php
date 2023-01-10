49b818c315e36a13ab47afd94a94fde01dd148f7
FabioBabaglioni <fabiobabaglioni@gmail.com>
2023-01-10 16:13:00 +0100

    Aggiunta form

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hotel</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 </head>
 <body>
 
@@ -50,6 +51,16 @@
 
 ];
     ?>
+
+    <head>
+        <form>
+            <span>Filtra alberghi che hanno il parcheggio:</span>
+            <input type="checkbox" name="parcheggio" id=""> <br>
+            <span>Filtra alberghi che hanno voto superiore a 2:</span>
+            <input type="checkbox" name="voto" id=""> <br>
+            <input type="submit" value="Filtra">
+        </form>
+    </head>
 
     <main>
         <?php
